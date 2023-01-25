135c5d2f45a26d78d2b59fb8a7e8471f9e78c6ee
Akif İnan Yiğit <87646106+wasny0ps@users.noreply.github.com>
2023-01-26 01:00:53 +0300

    Add files via upload

---
+++ SafeSignature.sol
@@ -0,0 +1,7 @@
+library SafeSignature{
+
+    function getSignature(address _signer, bytes8 _message) internal pure returns(bytes memory){
+        return abi.encodeWithSignature("getSignature(address,bytes8)", _signer, _message);
+    }
+
+}
